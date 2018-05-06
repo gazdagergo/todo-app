5b421358df447c9695b98ff789370ee8f2371d01
gazdagergo <info@gazdagergo.com>
2018-05-06 22:24:14 +0200

    use database key as id

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -4,7 +4,10 @@ const todos = (state = initialState, action) => {
   switch (action.type) { 
     case 'RECEIVE_TODOS':
       const { todos } = action;
-      return Object.keys(todos).map(key => todos[key])
+      return Object.keys(todos).map(key => ({
+        ...todos[key],
+        id: key
+      }));
 
     case 'TOGGLE_TODO':
       return state.map(todo =>
