6633346f498bd2a205936f003bcbccf212a121fe
gazdagergo <info@gazdagergo.com>
2018-05-06 22:25:00 +0200

    persist updateTodo

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,11 +17,22 @@ export const toggleTodo = id => ({
   id,
 });
 
-export const updateTodo = (id, text) => ({
-  type: "UPDATE_TODO",
-  id, 
-  text,
-});
+export const updateTodo = (id, text) => dispatch => {
+  fetch(`https://gg-todo-app.firebaseio.com/todos/${id}.json`, {
+    body: JSON.stringify({ text }),
+    method: 'PATCH',    
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json' },    
+    })
+    .then(response => response.json())
+    .then(json => dispatch({
+      type: "UPDATE_TODO",
+      id, 
+      text: json.text,
+    }
+  ))
+};
 
 export const removeTodo = id => ({
   type: "REMOVE_TODO",
